e0ba53e52a9cc95dfb0b25aab397ce6c317cc14d
guilherme <gui.dani13@gmail.com>
2022-06-12 19:23:39 -0300

    setup models

---
+++ lib/features/rick_and_morty/data/models/character_remote_data_source.dart
@@ -0,0 +1,9 @@
+import 'package:rick_and_morty_tdd/features/rick_and_morty/data/models/character_model.dart';
+
+abstract class CharacterRemoteDataSource {
+  /// Throws a [ServerException] for all error codes.
+  Future<CharacterModel> getConcreteCharacter(int number);
+
+  /// Throws a [ServerException] for all error codes.
+  // Future<CharacterModel> getAllCharacters();
+}
