c6cda45b8421975826adfc5645b1e8d618e6835d
guilherme <gui.dani13@gmail.com>
2022-06-12 18:25:02 -0300

    setup core folder

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,7 +1,11 @@
 import 'package:equatable/equatable.dart';
 
-class Failure extends Equatable{
-  @override 
+class Failure extends Equatable {
+  @override
   List<Object?> get props => [];
-
 }
+
+// General failures
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
