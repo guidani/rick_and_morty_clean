632f8ef5225c72a9b9b9a5ad232eaa4e678458a6
guilherme daniel <gui.dani13@gmail.com>
2022-06-29 10:39:15 -0300

    atualizado pacote para null safety

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,9 +1,9 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rick_and_morty_tdd/core/network/network_info.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+class MockDataConnectionChecker extends Mock implements InternetConnectionChecker {}
 
 void main() {
   final mockDataConnectionChecker = MockDataConnectionChecker();
